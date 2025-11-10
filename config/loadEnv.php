a17bbd5176a890b38b3b2e1d3ea50228cba762bb
Leonardo Martinelli <martinelliunivap@gmail.com>
2025-11-10 13:52:52 -0300

    loaderEnv

---
+++ config/loadEnv.php
@@ -0,0 +1,12 @@
+<?php
+
+$envPath = __DIR__ . '/../.env';
+
+if (file_exists($envPath)) {
+    $lines = file($envPath, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+    foreach ($lines as $line) {
+        if (strpos(trim($line), '#') === 0) continue;
+        putenv(trim($line));
+    }
+}
